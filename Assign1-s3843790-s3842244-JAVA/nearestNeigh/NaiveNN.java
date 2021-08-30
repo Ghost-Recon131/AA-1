b4054979a7154ce555d2f7cb60cea74873eed620
Jingxuan Feng <henryfeng24@pm.me>
2021-08-30 19:14:35 +1000

    Implemented buildIndex & search

--- Assign1-s3843790-s3842244-JAVA/nearestNeigh/NaiveNN.java
+++ Assign1-s3843790-s3842244-JAVA/nearestNeigh/NaiveNN.java
@@ -1,27 +1,51 @@
 package nearestNeigh;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * This class is required to be implemented.  Naive approach implementation.
+ * This class is required to be implemented. Naive approach implementation.
  *
- * 
+ *
  */
-public class NaiveNN implements NearestNeigh{
-    Point point = new Point(); // create new instance of Point class
-    ArrayList<Point> ListofPoints = new ArrayList<>(); // create arraylist to store all the points
+public class NaiveNN implements NearestNeigh {
+    // create new instance of Point class
+    Point point = new Point();
+    // create arraylist to store all the points
+    List<Point> pointsList = new ArrayList<>();
 
     @Override
     public void buildIndex(List<Point> points) {
-        ListofPoints = (ArrayList<Point>) points; // Store the points in an unsorted arraylist
-        //may need more work here
+        pointsList = points; // copy
     }
 
     @Override
     public List<Point> search(Point searchTerm, int k) {
-        // To be implemented.
-        return new ArrayList<>();
+        List<Point> searchedPointsList = pointsList;
+        List<Point> sortedSearchedPointsList = new ArrayList<>();
+
+        for (int i = 0; i < searchedPointsList.size(); i++) {
+            searchedPointsList.get(i).setDistance(searchedPointsList.get(i).distTo(searchTerm));
+        }
+
+        for(int i=0;i<searchedPointsList.size()-1;i++){
+            int m = i;
+            for(int j=i+1;j<searchedPointsList.size();j++){
+                if(searchedPointsList.get(m).getDistance() > searchedPointsList.get(j).getDistance())
+                    m = j;
+            }
+            //swapping elements at position i and m
+            Point temp = searchedPointsList.get(i);
+            searchedPointsList.set(i, searchedPointsList.get(m));
+            searchedPointsList.set(m, temp);
+        }
+
+        for (int i = 0; i < k ; i++) {
+            sortedSearchedPointsList.add(searchedPointsList.get(i));
+        }
+
+        return sortedSearchedPointsList;
     }
 
     @Override
