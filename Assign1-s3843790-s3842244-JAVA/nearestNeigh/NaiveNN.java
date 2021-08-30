5598aef784704987e803e558ff85d276c4ed81ee
Jingxuan Feng <henryfeng24@pm.me>
2021-08-30 22:48:25 +1000

    Finished implementing rest of methods + debugged Search method

--- Assign1-s3843790-s3842244-JAVA/nearestNeigh/NaiveNN.java
+++ Assign1-s3843790-s3842244-JAVA/nearestNeigh/NaiveNN.java
@@ -1,7 +1,6 @@
 package nearestNeigh;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -22,13 +21,14 @@ public class NaiveNN implements NearestNeigh {
 
     @Override
     public List<Point> search(Point searchTerm, int k) {
-        List<Point> searchedPointsList = pointsList;
-        List<Point> sortedSearchedPointsList = new ArrayList<>();
+        List<Point> searchedPointsList = pointsList; // ArrayList for storing points
+        List<Point> sortedSearchedPointsList = new ArrayList<>(); // ArrayList for storing sorted points
 
-        for (int i = 0; i < searchedPointsList.size(); i++) {
+        for (int i = 0; i < searchedPointsList.size(); i++) { // Calculate the distance for every point in arraylist
             searchedPointsList.get(i).setDistance(searchedPointsList.get(i).distTo(searchTerm));
         }
 
+        // This nested for loop sorts distance in ascending order from the searchTerm
         for(int i=0;i<searchedPointsList.size()-1;i++){
             int m = i;
             for(int j=i+1;j<searchedPointsList.size();j++){
@@ -41,8 +41,11 @@ public class NaiveNN implements NearestNeigh {
             searchedPointsList.set(m, temp);
         }
 
-        for (int i = 0; i < k ; i++) {
-            sortedSearchedPointsList.add(searchedPointsList.get(i));
+        // Checks that the points are of the same category, then adds to another arraylist
+        for (int i = 0; i <= k ; i++) {
+            if(searchedPointsList.get(i).categoryIsSame(searchTerm)){
+                sortedSearchedPointsList.add(searchedPointsList.get(i));
+            }
         }
 
         return sortedSearchedPointsList;
@@ -50,19 +53,32 @@ public class NaiveNN implements NearestNeigh {
 
     @Override
     public boolean addPoint(Point point) {
-        // To be implemented.
-        return false;
+        if (isPointIn(point)) { //check that point is not already existing, then adds it
+            return false;
+        } else {
+            pointsList.add(point);
+            return true;
+        }
     }
 
     @Override
-    public boolean deletePoint(Point point) {
-        // To be implemented.
-        return false;
+    public boolean deletePoint(Point point) { //check that point exists, then removes it
+        if (isPointIn(point)) {
+            pointsList.remove(point);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     @Override
-    public boolean isPointIn(Point point) {
-        // To be implemented.
+    public boolean isPointIn(Point point) { //check that point exists in the data structure
+        for (int i = 0; i < pointsList.size(); i++) {
+            if (pointsList.get(i).equals(point)) {
+                return true;
+            }
+        }
         return false;
     }
 
